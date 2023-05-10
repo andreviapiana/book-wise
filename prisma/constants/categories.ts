8c8b181487a89356cf471a70d7a9073f0938fb1a
André Viapiana <andreviapiana@gmail.com>
2023-05-10 09:51:42 -0300

    🐛fix: Correção na Ordem das Categorias no DB (ordem alfabética)

--- prisma/constants/categories.ts
+++ prisma/constants/categories.ts
@@ -1,31 +1,39 @@
 export const categories = [
   {
-    name: 'Programação',
-    id: 'c9f22067-4978-4a24-84a1-7d37f343dfc2',
+    name: 'Alegoria',
+    id: 'a1d0ee25-9c9a-49c8-84eb-7af1e0dd356d',
   },
   {
-    name: 'Educação',
-    id: 'f1a50507-0aa7-4245-8a5c-0d0de14e9d6d',
+    name: 'Arquitetura',
+    id: 'a2891eaa-6d9e-48d8-a86a-10aa017d3d3f',
   },
   {
-    name: 'Ficção',
-    id: '8c4a4a4d-cbc4-4d2c-bb46-e95b0a536e09',
+    name: 'Autoajuda',
+    id: 'a4d63d4e-f8ad-4a60-b7b9-9d925a2a8a92',
   },
   {
     name: 'Aventura',
     id: 'e9c6d3f6-f3ec-4c52-ae28-6adcbab6ee67',
   },
   {
-    name: 'Geek',
-    id: '2e65c193-325a-40c3-98f3-6c13e9b75b02',
-  },
-  {
-    name: 'Alegoria',
-    id: 'a1d0ee25-9c9a-49c8-84eb-7af1e0dd356d',
+    name: 'Educação',
+    id: 'f1a50507-0aa7-4245-8a5c-0d0de14e9d6d',
   },
   {
     name: 'Fábula',
     id: '997f8a10-21fb-4c80-bd16-17e8b79a31a3',
+  },
+  {
+    name: 'Ficção',
+    id: '8c4a4a4d-cbc4-4d2c-bb46-e95b0a536e09',
+  },
+  {
+    name: 'Geek',
+    id: '2e65c193-325a-40c3-98f3-6c13e9b75b02',
+  },
+  {
+    name: 'Programação',
+    id: 'c9f22067-4978-4a24-84a1-7d37f343dfc2',
   },
   {
     name: 'Romance',
@@ -34,14 +42,6 @@ export const categories = [
   {
     name: 'Suspense',
     id: '7c8dc74a-2e03-4d72-96de-822e332e5530',
-  },
-  {
-    name: 'Autoajuda',
-    id: 'a4d63d4e-f8ad-4a60-b7b9-9d925a2a8a92',
-  },
-  {
-    name: 'Arquitetura',
-    id: 'a2891eaa-6d9e-48d8-a86a-10aa017d3d3f',
   },
   {
     name: 'Terror',
